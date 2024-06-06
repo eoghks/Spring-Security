48d07c2eac2bc2e5ffbad00c4baf700c7b849cf9
eoghks <zhfldk7316@naver.com>
2024-06-06 16:20:26 +0900

    add Users Entity

---
+++ SpringSecurity-Example/src/main/java/com/example/SpringSecurity_Example/model/Users.java
@@ -0,0 +1,18 @@
+package com.example.SpringSecurity_Example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Users {
+	@Id
+	@Column(name ="user_id", columnDefinition ="int")
+	private Long id;
+	@Column(nullable = false)
+	private String password;
+}
