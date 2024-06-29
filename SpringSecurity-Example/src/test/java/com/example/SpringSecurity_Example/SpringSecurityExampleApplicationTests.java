fe85e4dab16b22c45fb6fc08b51d396c684c7529
eoghks <zhfldk7316@naver.com>
2024-06-29 22:51:47 +0900

    Back up SpringSecurityExampleApplicationTests

---
+++ SpringSecurity-Example/src/test/java/com/example/SpringSecurity_Example/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurity_Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
