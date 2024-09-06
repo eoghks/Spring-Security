d567460bd19c8542749fad423a8f9441fdea1268
eoghks <zhfldk7316@naver.com>
2024-09-06 16:10:29 +0900

    1. 등록된 Spring Bean 출력 기능 추가(현재 주석 처리)

--- SpringSecurity-Example/src/main/java/com/example/SpringSecurity_Example/SpringSecurityExampleApplication.java
+++ SpringSecurity-Example/src/main/java/com/example/SpringSecurity_Example/SpringSecurityExampleApplication.java
@@ -1,13 +1,30 @@
 package com.example.SpringSecurity_Example;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @SpringBootApplication
-public class SpringSecurityExampleApplication {
+public class SpringSecurityExampleApplication implements ApplicationContextAware {
+	@Autowired
+	private static ApplicationContext context;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityExampleApplication.class, args);
+		//printBean();
 	}
 
+	public static void printBean()  {
+		for(String bean: context.getBeanDefinitionNames()) {
+			System.out.println("SpringBoot Bean: "+ bean);
+		}
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		context = applicationContext;
+	}
 }
