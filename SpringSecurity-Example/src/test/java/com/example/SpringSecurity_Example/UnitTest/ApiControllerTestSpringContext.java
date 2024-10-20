6582d48c633c78b7da4b1338fca4f53aeb4f8120
eoghks <zhfldk7316@naver.com>
2024-10-21 04:36:16 +0900

    1. Remove @AutoConfigureJsonTesters

--- SpringSecurity-Example/src/test/java/com/example/SpringSecurity_Example/UnitTest/ApiControllerTestSpringContext.java
+++ SpringSecurity-Example/src/test/java/com/example/SpringSecurity_Example/UnitTest/ApiControllerTestSpringContext.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -35,7 +34,6 @@ import com.example.SpringSecurity_Example.service.MemberService;
 import com.google.gson.Gson;
 
 @WebMvcTest(ApiController.class)
-@AutoConfigureJsonTesters
 @WithMockUser
 public class ApiControllerTestSpringContext {
 	@MockBean
