77a1671276b1ee3858acaa635dec32c3b3d73c34
eoghks <zhfldk7316@naver.com>
2024-10-20 22:06:54 +0900

    1. WebMvcTest 어노테이션을 이용한 Controller Test 작성 예시 추가   1) 회원가입 성공 테스트 작성   2) 로그인 성공 테스트 작성   3) 인코딩 UTF8로 변경하는 코드 추가   4) 인증 처리 추가(csrf)   5) 인가(권한) 처리 추가(@WithMockUser)   6) any() 함수 수정(org.hamcrest.CoreMatchers -> org.mockito.ArgumentMatchers.any)

---
+++ SpringSecurity-Example/src/test/java/com/example/SpringSecurity_Example/UnitTest/ApiControllerTestSpringContext.java
@@ -0,0 +1,120 @@
+package com.example.SpringSecurity_Example.UnitTest;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.example.SpringSecurity_Example.controller.ApiController;
+import com.example.SpringSecurity_Example.model.constant.MessageEnum;
+import com.example.SpringSecurity_Example.model.dto.MemberDto;
+import com.example.SpringSecurity_Example.model.vo.SingUpResultVo;
+import com.example.SpringSecurity_Example.service.MemberService;
+import com.google.gson.Gson;
+
+@WebMvcTest(ApiController.class)
+@AutoConfigureJsonTesters
+@WithMockUser
+public class ApiControllerTestSpringContext {
+	@MockBean
+	private MemberService memberServie;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@BeforeEach
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+			.addFilters(new CharacterEncodingFilter("UTF-8", true))
+			.build();
+	}
+
+	@Test
+	@DisplayName("회원 가입 성공 Test <Spring Context 사용>")
+	void signUpSuccess() throws Exception {
+		//given -> 테스트 데이터 생성
+		MemberDto request = getSignUpRequest();
+		SingUpResultVo expectResponse = getSignUpResponse();
+
+		doNothing().when(memberServie).createUser(any());
+
+		//when -> 수행
+		ResultActions resultActions = mockMvc.perform(
+				MockMvcRequestBuilders.post("/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new Gson().toJson(request))
+				.with(csrf()));
+
+		//then -> 결과 비교
+		MvcResult mvcResult = resultActions
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("loginId").exists())
+				.andExpect(jsonPath("msg").exists())
+				.andExpect(jsonPath("msg", is(expectResponse.getMsg())))
+				.andDo(print())
+				.andReturn();
+		System.out.println("mvcResult : " + mvcResult.getResponse().getContentAsString());
+	}
+
+	@Test
+	@DisplayName("로그인 성공 Test <Spring Context 사용>")
+	void LoginSuccess() throws Exception {
+		//given -> 테스트 데이터 생성
+		MemberDto request = getSignUpRequest();
+		String response = "Login Success";
+
+		given(memberServie.login(any())).willReturn(response);
+
+		//when -> 수행
+		ResultActions resultActions = mockMvc.perform(
+				MockMvcRequestBuilders.post("/login")
+				.content(new Gson().toJson(request))
+				.contentType(MediaType.APPLICATION_JSON)
+				.with(csrf()));
+
+		//then -> 결과 비교
+		MvcResult mvcResult = resultActions
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("loginId").exists())
+				.andDo(print())
+				.andReturn();
+
+		System.out.println("mvcResult : " + mvcResult.getResponse().getContentAsString());
+	}
+
+	private MemberDto getSignUpRequest() {
+		MemberDto dto = new MemberDto();
+		dto.setLoginId("unitTest2");
+		dto.setPassword("unitTest2");
+		return dto;
+	}
+
+	private SingUpResultVo getSignUpResponse() {
+		return new SingUpResultVo("unitTest", MessageEnum.SignUpSuccess.getMsg());
+	}
+}
